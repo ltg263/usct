8728496fb89dc6027f43078af4698ad2b5a5f507
raozuchang <935021148@qq.com>
2019-12-15 16:43:13 +0800

    解决后端返回整数不带小数点时引发的打开"我的"页面闪退问题

--- easy_pay/app/src/main/java/com/frico/easy_pay/ui/activity/me/wallet/NewWalletActivity.java
+++ easy_pay/app/src/main/java/com/frico/easy_pay/ui/activity/me/wallet/NewWalletActivity.java
@@ -349,7 +349,11 @@ public class NewWalletActivity extends BaseActivity implements ActionBarClickLis
         String after = "";
         String str1 = Util.splitSctCount(before)[0];
         String str2 = Util.splitSctCount(before)[1];
-        str2 = str2.substring(0,2);
+        if (str2.lenth()>=2){
+            str2 = str2.substring(0,2);
+        }else {
+            str2 = "00"
+        }
         after = str1+"."+str2;
         return after;
     }

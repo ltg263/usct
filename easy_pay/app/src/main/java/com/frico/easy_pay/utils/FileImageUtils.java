d04bddecbb9b76bcf56ef115c7a5b2d4ecfa9253
litongge <123456789>
2020-01-08 14:51:16 +0800

    团队；收款方式删除功能

---
+++ easy_pay/app/src/main/java/com/frico/easy_pay/utils/FileImageUtils.java
@@ -0,0 +1,34 @@
+package com.frico.easy_pay.utils;
+
+import android.graphics.Bitmap;
+import android.os.Environment;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileImageUtils {
+    public static File saveBitmapFile(Bitmap bitmap) {
+
+        File file = new File(Environment.getExternalStorageDirectory().getPath() + "/skm.jpg");
+        if(file.exists()){
+            file.delete();
+        }
+        try {
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+
+            bos.flush();
+            bos.close();
+
+            return file;
+        } catch (IOException e) {
+            LogUtils.w(":"+e.toString());
+            e.printStackTrace();
+        }
+
+        return file;
+    }
+}
